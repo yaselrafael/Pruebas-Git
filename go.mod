cfa08c27519629dfc82a70c855660b69a9d2d9d6
yaselrafael <yaselrafael@gmail.com>
2024-01-12 14:58:50 -0500

    Trabajo con arreglo, slices, slices Rango

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yaselrafael/gocurso
+
+go 1.21.5
+
+require golang.org/x/exp v0.0.0-20240112132812-db7319d0e0e3 // indirect
--- helloword/hello.go
+++ helloword/hello.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	//"golang.org/x/exp/slices"
+)
+
+var name string = "Yasel"
 
 func main() {
 	fmt.Printf("hello, world\n")
 
+	fmt.Println(name)
+
+	var age = [3]int{25, 30, 35}
+
+	//array
+	var name =[4]string{"pepe", "antonio", "juan", "luigi"}
+		//name[1]= "pech"
+	fmt.Println(age, len(age))
+	fmt.Println(name, len(name))
+
+//slices
+var score =[]int{100, 50, 60}
+score[0]= 40
+
+score = append(score, 85)
+score = append(score, 200)
+fmt.Println(score, len(score))
+
+//slices Range
+rango1 := name[1:3]
+rango2 := name[2:]   //despues del 2
+rango3 := name[:3]    //antes del 3
+fmt.Println(rango1)
+fmt.Println(rango1, rango2, rango3)
+rango1 = append(rango1, "koopa")
+
+fmt.Println(rango1)
 
 }
