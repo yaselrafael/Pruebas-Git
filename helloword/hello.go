1e5ddd82b40f310e99816de4cae177e50ceac761
yaselrafael <yaselrafael@gmail.com>
2024-01-13 18:33:56 -0800

    OTRA PRUEBA

--- helloword/hello.go
+++ helloword/hello.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-
 	//"golang.org/x/exp/slices"
 )
 
@@ -16,27 +15,27 @@ func main() {
 	var age = [3]int{25, 30, 35}
 
 	//array
-	var name =[4]string{"pepe", "antonio", "juan", "luigi"}
-		//name[1]= "pech"
+	var name = [4]string{"pepe", "antonio", "juan", "luigi"}
+	//name[1]= "pech"
 	fmt.Println(age, len(age))
 	fmt.Println(name, len(name))
 
-//slices
-var score =[]int{100, 50, 60}
-score[0]= 40
+	//slices
+	var score = []int{100, 50, 60}
+	score[0] = 40
 
-score = append(score, 85)
-score = append(score, 200)
-fmt.Println(score, len(score))
+	score = append(score, 85)
+	score = append(score, 200)
+	fmt.Println(score, len(score))
 
-//slices Range
-rango1 := name[1:3]
-rango2 := name[2:]   //despues del 2
-rango3 := name[:3]    //antes del 3
-fmt.Println(rango1)
-fmt.Println(rango1, rango2, rango3)
-rango1 = append(rango1, "koopa")
+	//slices Range
+	rango1 := name[1:3]
+	rango2 := name[2:] //despues del 2
+	rango3 := name[:3] //antes del 3
+	fmt.Println(rango1)
+	fmt.Println(rango1, rango2, rango3)
+	rango1 = append(rango1, "koopa")
 
-fmt.Println(rango1)
+	fmt.Println(rango1)
 
 }
